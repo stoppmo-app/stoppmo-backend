3f11952c74e2a7f3ac193de1cd5fdf7177726d5d
WilliamFernsV3 <williamfernsv3@gmail.com>
2024-12-22 21:32:23 +0200

    feat(UserController): done with controller for now
    TODOS:
    - Need to make migrations for User, Badge and UserBadge
    - Test UserController
    - Use .env file for database credentials, make sure it works

--- backend/Sources/App/Controllers/UserController.swift
+++ backend/Sources/App/Controllers/UserController.swift
@@ -6,32 +6,59 @@ struct UserController: RouteCollection {
         let users = routes.grouped("users")
 
         users.get(use: self.getAllUsers)
-        // users.post(use: self.create)
-        // users.group(":userID") { todo in
-        //     todo.delete(use: self.delete)
-        // }
+        users.post(use: self.create)
+
+        users.group(":userID") { user in
+            user.get(use: self.getUserInfo)
+            user.delete(use: self.deleteUser)
+            user.put(use: self.updateUser)
+        }
     }
 
     @Sendable
-    func getAllUsers(req: Request) async throws -> [TodoDTO] {
+    func getAllUsers(req: Request) async throws -> [UserDTO] {
         try await User.query(on: req.db).all().map { $0.toDTO() }
     }
 
-    // @Sendable
-    // func create(req: Request) async throws -> TodoDTO {
-    //     let todo = try req.content.decode(TodoDTO.self).toModel()
+    @Sendable
+    func create(req: Request) async throws -> UserDTO {
+        let user = try req.content.decode(UserDTO.self).toModel()
 
-    //     try await todo.save(on: req.db)
-    //     return todo.toDTO()
-    // }
+        try await user.save(on: req.db)
+        return user.toDTO()
+    }
 
-    // @Sendable
-    // func delete(req: Request) async throws -> HTTPStatus {
-    //     guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
-    //         throw Abort(.notFound)
-    //     }
+    @Sendable
+    func getUserInfo(req: Request) async throws -> UserDTO {
+        guard let user = try await User.find(req.parameters.get("userID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        return user.toDTO()
+    }
 
-    //     try await todo.delete(on: req.db)
-    //     return .noContent
-    // }
+    @Sendable
+    func deleteUser(req: Request) async throws -> HTTPStatus {
+        guard let user = try await User.find(req.parameters.get("userID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+
+        try await user.delete(on: req.db)
+        return .noContent
+    }
+
+    @Sendable
+    func updateUser(req: Request) async throws -> UserDTO {
+        let updatedUser = try req.content.decode(UserDTO.self)
+        guard let user = try await User.find(req.parameters.get("userID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+
+        user.name = updatedUser.name ?? user.name
+        user.surname = updatedUser.surname ?? user.surname
+        user.password = updatedUser.password ?? user.password
+        user.userRole = updatedUser.userRole ?? user.userRole
+
+        try await user.update(on: req.db)
+        return user.toDTO()
+    }
 }
