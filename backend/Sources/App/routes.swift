07528cc9c2b1d1f8b80165d0eea948d9bdb8b993
WilliamFernsV3 <williamfernsv3@gmail.com>
2024-12-22 11:39:47 +0200

    feat(DevDockerfile): development container with hot reloading fully implemented

--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -3,7 +3,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello 1234 Vapor!"])
+        try await req.view.render("index", ["title": "Hello Vapor!"])
     }
 
     app.get("hello") { req async -> String in
