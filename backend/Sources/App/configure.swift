958df11b4775df10769f63600112b663a573f9bd
WilliamFernsV3 <williamfernsv3@gmail.com>
2024-12-27 09:08:23 +0200

    feat(fly-review.toml): review app action created
    Use `DATABASE_URL` to connect to database in productioin (Fly automatically set the `DATABASE_URL` if a database was created with the app using `fly launch`.)

--- backend/Sources/App/configure.swift
+++ backend/Sources/App/configure.swift
@@ -9,14 +9,20 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    // In production environment, it will use DATABASE_URL
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        app.logger.info("Database URL: \(databaseURL)")
+        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+    } else {
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    }
 
     app.views.use(.leaf)
 
