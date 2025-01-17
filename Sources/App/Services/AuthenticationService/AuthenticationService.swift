31544929204e7b300b2aa79f8638881a97677c07
WilliamFernsV3 <williamfernsv3@gmail.com>
2025-01-17 11:25:32 +0200

    feat(AuthenticationService,User,AuthenticationController,AuthenticationTypes): changed authentication to use bearer auth instead

--- Sources/App/Services/AuthenticationService/AuthenticationService.swift
+++ Sources/App/Services/AuthenticationService/AuthenticationService.swift
@@ -6,15 +6,16 @@ struct AuthenticationService {
 
     // TODO: add some two step verification to verify the email belongs to the creator
 
-    func login(user: User) async throws -> UserToken {
+    func login(user: User) async throws -> String {
         let token = try self.generateToken(for: user)
         try await token.save(on: db)
-        return token
+        return token.value
     }
 
-    func logout(user userID: UUID) async throws {
+    func logout(user userID: UUID, req: Request) async throws {
         // TODO: send messages to message service here
         try await self.removeOldTokens(for: userID)
+        req.auth.logout(User.self)
     }
 
     func generateToken(for user: User) throws -> UserToken {
@@ -24,8 +25,26 @@ struct AuthenticationService {
         )
     }
 
-    static func isTokenValid(token: UserToken) -> Bool {
-        token.expiresAt > Date()
+    func getUserFromToken(_ token: String) async throws -> User {
+        // 1. Get token from db
+        let token = try await getToken(token)
+        // 2. validate token
+        if token.isTokenValid() == false {
+            throw Abort(.custom(code: 401, reasonPhrase: "Token Expired"))
+        }
+        // 3. return user
+        return token.user
+    }
+
+    func getToken(_ token: String) async throws -> UserToken {
+        guard let userToken = try await UserToken
+            .query(on: db)
+            .filter(\.$value, .equal, token)
+            .first()
+        else {
+            throw Abort(.custom(code: 401, reasonPhrase: "Token Not Found"))
+        }
+        return userToken
     }
 
     func removeOldTokens(for userID: UUID) async throws {
